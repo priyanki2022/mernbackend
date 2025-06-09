40ae951860ca24bad8f8bb65f81165417768656d
rajeshrathwa <rajeshrathwa15@gmail.com>
2025-06-09 11:33:37 +0530

    Use for env for render

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ app.get('/videos', async (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
